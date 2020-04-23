b63f8e73cb2e26850d97fc45a996748bb7f6cfb2
ivanglinka20@gmail.com <ivanglinka20@gmail.com>
2020-04-23 15:55:30 +0300

    addphp

---
+++ php/telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot1033013361:AAFI3NsETDu9TKClWfVkogo0txrgtIoWmwU/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$message = $_POST['message'];
+$token = "1033013361:AAFI3NsETDu9TKClWfVkogo0txrgtIoWmwU";
+$chat_id = "531631855";
+$arr = array(
+    'Телефон: ' => $phone,
+    'Email' => $email
+    'Повідомлення: ' => $message,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
