b5047f90fb9d411c7ff227bb23fa8824c5eb9de8
ivanglinka20@gmail.com <ivanglinka20@gmail.com>
2020-04-24 01:01:26 +0300

    save

--- telegram.php
+++ telegram.php
@@ -1,17 +1,17 @@
 <?php
 
-/* https://api.telegram.org/bot1033013361:AAFI3NsETDu9TKClWfVkogo0txrgtIoWmwU/getUpdates,
+/* https://api.telegram.org/bot1108154460:AAGCb0pT16Cw6CVzf7L81CtgqzT2yU42Ycs/getUpdates,
 где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
 
 $email = $_POST['user_email'];
 $phone = $_POST['user_phone'];
 $message = $_POST['user_message'];
-$token = "1033013361:AAFI3NsETDu9TKClWfVkogo0txrgtIoWmwU";
+$token = "1108154460:AAGCb0pT16Cw6CVzf7L81CtgqzT2yU42Ycs";
 $chat_id = "-373247226";
 $arr = array(
-  'Email: ' => $email
+  'Email: ' => $email,
   'Телефон: ' => $phone,
-  'Повідомлення: ' => $message,
+  'Повідомлення: ' => $message
 );
 
 foreach($arr as $key => $value) {
@@ -20,9 +20,9 @@ foreach($arr as $key => $value) {
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-if ($sendToTelegram) {
-  header('Location: thank-you.html');
-} else {
-  echo "Error";
-}
+// if ($sendToTelegram) {
+//   header('Location: index.html#home');
+// } else {
+//   echo "Error";
+// }
 ?>
